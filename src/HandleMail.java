f24c58f8508445db1e774571e33c195194b1f9da
Anushka Srivastava <47708011+Anushri-glitch@users.noreply.github.com>
2023-04-16 22:09:29 +0530

    Create HandleMail.java

---
+++ src/HandleMail.java
@@ -0,0 +1,48 @@
+package org.geekster;
+
+import org.example.MailAuthenticator;
+import org.example.MailConstants;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class HandleMail {
+
+    void sendMail(){
+
+        String host  = "smtp.gmail.com";
+
+        Properties props = System.getProperties();
+
+        System.out.println(props);
+
+        //set properties :
+
+        props.put("mail.smtp.host",host);
+        props.put("mail.smtp.port","465");
+        props.put("mail.smtp.ssl.enable","true");
+        props.put("mail.smtp.auth","true");
+
+
+        Session mailSession = Session.getInstance(props,new MailAuthenticator());
+
+        MimeMessage mimeMessage = new MimeMessage(mailSession);
+
+        try {
+            mimeMessage.setFrom(MailConstants.SENDER);
+
+            mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(MailConstants.RECEIVERMAILADDRESS));
+
+            mimeMessage.setSubject(MailConstants.SUBJECT);
+
+            mimeMessage.setText(MailConstants.MESSAGE);
+
+            Transport.send(mimeMessage);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
