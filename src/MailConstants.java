0a8a9843824f7635696e360cb4607b9410d4bf3a
Anushka Srivastava <47708011+Anushri-glitch@users.noreply.github.com>
2023-04-16 22:13:32 +0530

    Create MailConstants.java

---
+++ src/MailConstants.java
@@ -0,0 +1,12 @@
+package org.geekster;
+
+public class MailConstants {
+
+    public static final String SENDER = "shrishtisrivastava19@gmail.com";
+
+    public static final String RECEIVERMAILADDRESS = "harshsikarwar125@gmail.com";
+
+    public static final String SUBJECT = "Testing Java mail : 12-April-2023";
+
+    public static final String MESSAGE = "Hello harsh if you java notes can you please send me...!!";
+}
