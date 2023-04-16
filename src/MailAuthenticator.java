5453b041f9fd58ee2ad99b802e2d7988aacc9b07
Anushka Srivastava <47708011+Anushri-glitch@users.noreply.github.com>
2023-04-16 22:10:41 +0530

    Create MailAuthenticator.java

---
+++ src/MailAuthenticator.java
@@ -0,0 +1,13 @@
+package org.geekster;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+public class MailAuthenticator extends Authenticator {
+
+    @Override
+    protected PasswordAuthentication getPasswordAuthentication(){
+
+        return new PasswordAuthentication(MailConstants.SENDER, "lomudhsxzpczcuhd");
+    }
+}
